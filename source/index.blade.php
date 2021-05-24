ed5a4f43accec2f6cba67cf9a8b29c25f5d8533e
Jethro May <jethro.may@minttwist.com>
2021-05-24 14:17:26 +0200

    Updated index.blade.php

--- source/index.blade.php
+++ source/index.blade.php
@@ -2,6 +2,6 @@
 
 @section('body')
 <div class="p-8">
-    <h1 class="text-3xl font-bold">Hello world!</h1>
+    <h1 class="text-3xl font-bold">jethromay.com - 2021 refresh</h1>
 </div>
 @endsection
