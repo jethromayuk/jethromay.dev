a9b25fd38d6f6f4a410b689abfcbe639948a27a0
Jethro May <jethro.may@minttwist.com>
2021-05-25 18:32:34 +0200

    Updated and cleaned up markup of header/nav

--- source/_layouts/main.blade.php
+++ source/_layouts/main.blade.php
@@ -9,39 +9,37 @@
         <link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}">
         <script defer src="{{ mix('js/main.js', 'assets/build') }}"></script>
     </head>
-    <body class="text-gray-900 font-sans antialiased">
-        <header class="bg-white border-b-2 fixed shadow-none top-0 z-50 w-full lg:border-0 lg:p-2">
-            <div class="flex flex-wrap items-center container mx-auto w-full mt-0 px-4 py-3">
-                <div class="flex items-center justify-between">
-                    <a href="/" class="flex font-bold text-xl no-underline hover:text-blue-700 hover:no-underline" title="Jethro May">
-                        Jethro May
-                    </a>
-                </div>
-                <nav class="flex flex-grow" aria-label="Main">
-                    <ul class="flex flex-1 justify-end space-x-6 lg:px-0" role="menubar">
-                        <li class="text-base py-1 md:text-lg lg:py-2" role="menuitem">
-                            <a href="/blog" class="text-gray-700 hover:text-blue-700 lg:text-lg">Blog</a>
+    <body class="text-gray-900 font-sans antialiased container mx-auto max-w-screen-lg">
+        <header class="py-12">
+            <div class="grid grid-cols-2 items-center">
+                <a href="/" class="text-xl font-bold hover:text-blue-700" title="Jethro May">
+                    Jethro May
+                </a>
+                <nav aria-label="Main">
+                    <ul class="flex items-center justify-end space-x-6 text-lg text-gray-700" role="menubar">
+                        <li role="menuitem">
+                            <a href="/blog" class="hover:text-blue-700">Blog</a>
                         </li>
-                        <li class="text-base py-1 md:text-lg lg:py-2" role="menuitem">
-                            <a href="/snippets" class="text-gray-700 hover:text-blue-700 lg:text-lg">Snippets</a>
+                        <li role="menuitem">
+                            <a href="/snippets" class="hover:text-blue-700">Snippets</a>
                         </li>
-                        <li class="text-base py-1 md:text-lg lg:py-2" role="menuitem">
-                            <a href="/uses" class="text-gray-700 hover:text-blue-700 lg:text-lg">Uses</a>
+                        <li role="menuitem">
+                            <a href="/uses" class="hover:text-blue-700">Uses</a>
                         </li>
-                        <li class="text-base py-1 md:text-lg lg:py-2" role="menuitem">
-                            <a href="/contact" class="text-gray-700 hover:text-blue-700 lg:text-lg">Contact</a>
+                        <li role="menuitem">
+                            <a href="/contact" class="hover:text-blue-700">Contact</a>
                         </li>
                     </ul>
                 </nav>
             </div>
         </header>
-        <main>
+        <main id="main" role="main">
             @yield('body')
         </main>
-        <footer class="flex-shrink-0 pt-0 pb-10 pr-5 pl-5 xs:text-center sm:text-center" role="contentinfo">
+        <footer class="flex-shrink-0 pt-0 pb-10 pr-5 pl-5 text-center" role="contentinfo">
             <div class="flex flex-col items-center justify-center">
                     <p class="text-sm mb-1 text-gray-500">
-                        <time class="text-gray-500">© {{ $page->date->format('Y') }}</time> • Copyright Jethro May. All rights reserved.
+                        <time class="text-gray-500">© {{ $page->date }}</time> • Copyright Jethro May. All rights reserved.
                     </p>
             </div>
             <div class="flex items-center justify-center">
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   mode: 'jit',
   purge: [
@@ -7,6 +9,16 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    theme: {
+        colors: {
+          transparent: 'transparent',
+          current: 'currentColor',
+          black: colors.black,
+          white: colors.white,
+          gray: colors.trueGray,
+          blue: colors.blue
+        }
+      }
   },
   variants: {
     extend: {},
