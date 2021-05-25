7643d76d03ae07bc54e4fb2ec5904b3419bda790
Jethro May <jethro.may@minttwist.com>
2021-05-25 09:00:00 +0200

    Removed Carbon helper method in favour of php date() function

--- config.php
+++ config.php
@@ -1,12 +1,10 @@
 <?php
 
-use Carbon\Carbon;
-
 return [
     'production' => false,
     'baseUrl' => '',
     'title' => 'Jethro May - Full Stack PHP Developer',
     'description' => 'Jethro May is a Full-Stack PHP developer currently living and working in South Africa.',
     'collections' => [],
-    'date' => Carbon::now()
+    'date' => date('Y')
 ];
