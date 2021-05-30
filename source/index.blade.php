f7e27b4a02af0d03ca18f0809447e5badb2b21e7
Jethro May <jethro.may@minttwist.com>
2021-05-30 11:44:19 +0200

    Added GenerateSitemap and GenerateIndex event listeners

--- source/index.blade.php
+++ source/index.blade.php
@@ -5,18 +5,35 @@
         <section class="grid grid-cols-2" itemprop="articleSection">
             <div class="relative col-start-1 col-end-2 place-self-center">
                 <h2 class="font-bold text-gray-900 text-6xl tracking-tight -ml-1">Hey, I'm Jethro.</h2>
-                <p class="font-medium text-2xl my-4">I'm a South African Laravel developer, living and working in South Africa. I love tech, learning and sharing what I know.</p>
+                <p class="font-medium text-2xl my-4">I'm a South African Laravel developer, living and working in London. I love tech, learning and sharing what I know.</p>
             </div>
             <div class="relative col-span-auto place-self-end">
                 <img class="rounded-lg shadow-lg max-w-xs" src="/assets/images/me.jpg" alt="Jethro May">
             </div>
         </section>
-        @foreach ($posts as $post)
-            <li>
-                <a href="{{ $post->getUrl() }}">
-                    {{ $post->title }}
-                </a>
-        </li>
-        @endforeach
+        <section class="py-12">
+            @foreach ($posts as $post)
+                <article>
+                    <header class="flex flex-col">
+                        <h3 class="text-4xl leading-snug mr-3">
+                            <a href="{{ $post->getUrl() }}">
+                                {{ $post->title }}
+                            </a>
+                        </h3>
+                        <p class="text-sm text-gray-600 mb-6 mt-1">
+                            <time>January 2, 2021</time> • 3 min read
+                        </p>
+                    </header>
+                    <p class="text-xl">
+                        {{ $post->description }}
+                    </p>
+                    <footer class="mt-4">
+                        <a class="flex items-center text-xl text-green-500" href="/posts/2020-in-review/">
+                            Read More
+                        </a>
+                    </footer>
+                </article>
+            @endforeach
+        </section>
     </article>
 @endsection
