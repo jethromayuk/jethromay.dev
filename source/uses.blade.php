85bde7605e62f4b4ce5a3b59fa2743e314693030
Jethro May <jethro.may@minttwist.com>
2021-06-01 09:53:57 +0200

    Added basic content to /uses page

--- source/uses.blade.php
+++ source/uses.blade.php
@@ -1,7 +1,95 @@
+---
+title: Uses
+description: The list of blog posts for the site
+---
 @extends('_layouts.main')
 
 @section('body')
-<div class="p-8">
-    <h1 class="text-3xl font-bold">Uses</h1>
-</div>
-@endsection
+<article itemscope="" itemtype="http://schema.org/Article">
+    <header class="mb-3">
+        <h1 class="text-4xl font-bold">Uses</h1>
+    </header>
+    <p>
+        Inspired by Wes Bos, Freek Murze and many others I've put together this list of hardware, software and services that I use. I will keep this list updated as my workflow changes.
+    </p>
+
+    <h2 class="text-3xl">Hardware</h2>
+
+    <ul class="list-inside">
+        <li>
+            MacBook Pro (2019)
+
+This is my main development machine which I use for the majority of my work.
+        </li>
+    </ul>
+
+    <h2 class="text-3xl mb-3">Software</h2>
+
+    <ul class="list-inside list-disc mb-3">
+        <li>Visual Studio Code
+            <p>I used PHPStorm for many years but after purchasing Caleb Porzio's course, Make VS Code Awesome I made the switch and haven't looked back.</p>
+        </li>
+        <li>
+            TablePlus
+            <p></p>
+        </li>
+        <li>
+            iTerm 2
+            <p></p>
+        </li>
+        <li>
+            Tinkerwell
+            <p></p>
+        </li>
+        <li>
+            HELO
+            <p></p>
+        </li>
+        <li>
+            Expose
+            <p></p>
+        </li>
+    </ul>
+
+    <h2 class="text-3xl mb-3">Productivity</h2>
+
+    <ul class="list-inside list-disc mb-3">
+        <li>
+            Alfred 4 + Powerpack
+            <p>
+                I only recently started using Alfred and it has been a game-changer, I cannot imagine working without it.
+            </p>
+        </li>
+    </ul>
+
+    <h2 class="text-3xl mb-3">Design</h2>
+
+    <ul class="list-inside list-disc mb-3">
+        <li>
+            Figma
+            <p></p>
+        </li>
+        <li>
+            Sip
+            <p></p>
+        </li>
+    </ul>
+
+    <h2 class="text-3xl mb-3">Hosting</h2>
+
+    <ul class="list-inside list-disc mb-3">
+        <li>
+            GitHub
+            <p></p>
+        </li>
+        <li>
+            Netlify
+            <p></p>
+        </li>
+        <li>
+            Namecheap
+            <p></p>
+        </li>
+    </ul>
+</article>
+@stop
