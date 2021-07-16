dd9532b58efa2b110b992a4504d5915e937291e8
Jethro May <jethro.may@minttwist.com>
2021-07-16 13:40:24 +0200

    Updated link and hover colors

--- source/_components/footer.blade.php
+++ source/_components/footer.blade.php
@@ -5,8 +5,8 @@
         </p>
     </div>
     <div class="flex items-center justify-center">
-        <a href="https://twitter.com/jethromayuk" class="text-sm mr-2 ml-2 hover:text-green-700" target="_blank" rel="noopener noreferrer">Twitter</a>
-        <a href="https://github.com/jethromayuk" class="text-sm mr-2 ml-2 hover:text-green-700" target="_blank" rel="noopener noreferrer">GitHub</a>
-        <a href="https://jethromay.com/posts/index.xml" class="text-sm mr-2 ml-2 hover:text-green-700">RSS</a>
+        <a href="https://twitter.com/jethromayuk" class="text-sm mr-2 ml-2 hover:text-niagara" target="_blank" rel="noopener noreferrer">Twitter</a>
+        <a href="https://github.com/jethromayuk" class="text-sm mr-2 ml-2 hover:text-niagara" target="_blank" rel="noopener noreferrer">GitHub</a>
+        <a href="https://jethromay.com/posts/index.xml" class="text-sm mr-2 ml-2 hover:text-niagara">RSS</a>
     </div>
 </footer>
