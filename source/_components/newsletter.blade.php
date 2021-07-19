5062590bcf245aa240b6779be74bc6a8482545d1
Jethro May <jethro.may@minttwist.com>
2021-07-19 09:14:10 +0200

    Added Mailchimp subscription form

--- source/_components/newsletter.blade.php
+++ source/_components/newsletter.blade.php
@@ -10,16 +10,32 @@
                         Receive my latest content by email. No spam, ever.
                     </p>
                 </div>
-                <form action="#" class="mt-6 sm:mx-auto sm:max-w-lg sm:flex">
-                    <div class="min-w-0 flex-1">
-                        <label for="cta-email" class="sr-only">Email address</label>
-                        <input id="cta-email" type="email" class="block w-full border border-transparent rounded-md px-5 py-3 text-base text-gray-900 placeholder-gray-500 shadow-sm focus:outline-none focus:border-transparent focus:ring-2 focus:ring-offset-2 " placeholder="Enter your email">
-                    </div>
-                    <div class="mt-4 sm:mt-0 sm:ml-3">
-                        <button type="submit" class="block w-full rounded-md border px-5 py-3 bg-transparent border-white text-base font-medium text-white shadow hover:bg-white hover:text-madison focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 sm:px-10">Subscribe</button>
+                <form id="mc-embedded-subscribe-form" action="https://jethromay.us6.list-manage.com/subscribe/post?u=c882d43252516c87b01b2f8b2&amp;id=e9f86a5b12" method="post"  name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+                    <div id="mc_embed_signup_scroll" class="mt-6 sm:mx-auto sm:max-w-lg sm:flex">
+                        <div class="min-w-0 flex-1 mc-field-group">
+                            <label for="mce-EMAIL" class="sr-only">Email address</label>
+                            <input id="mce-EMAIL" type="email" value="" name="EMAIL" class="block w-full border border-transparent rounded-md px-5 py-3 text-base text-gray-900 placeholder-gray-500 shadow-sm focus:outline-none focus:border-transparent focus:ring-2 focus:ring-offset-2 " placeholder="Enter your email" required>
+                        </div>
+                        <div id="mce-responses" class="clear">
+                            <div class="response" id="mce-error-response" style="display:none"></div>
+                            <div class="response" id="mce-success-response" style="display:none"></div>
+                        </div>
+                        <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_c882d43252516c87b01b2f8b2_e9f86a5b12" tabindex="-1" value=""></div>
+                        <div class="mt-4 sm:mt-0 sm:ml-3">
+                            <button id="mc-embedded-subscribe" type="submit" value="Subscribe" name="subscribe" class="block w-full rounded-md border px-5 py-3 bg-transparent border-white text-base font-medium text-white shadow hover:bg-white hover:text-madison focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 sm:px-10">Subscribe</button>
+                        </div>
                     </div>
                 </form>
             </div>
         </div>
     </div>
 </section>
+
+
+{{-- <div id="mc_embed_signup">
+<form action="https://jethromay.us6.list-manage.com/subscribe/post?u=c882d43252516c87b01b2f8b2&amp;id=e9f86a5b12" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+    <div id="mc_embed_signup_scroll">
+
+    </div>
+</form>
+</div> --}}
