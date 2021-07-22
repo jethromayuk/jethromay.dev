1d754d53ac60f1dcd2c7dfcebf03e38a5845a43a
Jethro May <jethro.may@minttwist.com>
2021-07-22 09:53:36 +0200

    Added icons

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -30,9 +30,28 @@
 
         <meta name="monetization" content="$ilp.uphold.com/kHR3Z9ULUw2p">
 
+        <link rel="apple-touch-icon-precomposed" sizes="57x57" href="/assets/images/icons/apple-touch-icon-57x57.png" />
+        <link rel="apple-touch-icon-precomposed" sizes="114x114" href="/assets/images/icons/apple-touch-icon-114x114.png" />
+        <link rel="apple-touch-icon-precomposed" sizes="72x72" href="/assets/images/icons/apple-touch-icon-72x72.png" />
+        <link rel="apple-touch-icon-precomposed" sizes="144x144" href="/assets/images/icons/apple-touch-icon-144x144.png" />
+        <link rel="apple-touch-icon-precomposed" sizes="60x60" href="/assets/images/icons/apple-touch-icon-60x60.png" />
+        <link rel="apple-touch-icon-precomposed" sizes="120x120" href="/assets/images/icons/apple-touch-icon-120x120.png" />
+        <link rel="apple-touch-icon-precomposed" sizes="76x76" href="/assets/images/icons/apple-touch-icon-76x76.png" />
+        <link rel="apple-touch-icon-precomposed" sizes="152x152" href="/assets/images/icons/apple-touch-icon-152x152.png" />
+        <link rel="icon" type="image/png" href="/img/favicon-196x196.png" sizes="196x196" />
+        <link rel="icon" type="image/png" href="/img/favicon-96x96.png" sizes="96x96" />
+        <link rel="icon" type="image/png" href="/img/favicon-32x32.png" sizes="32x32" />
+        <link rel="icon" type="image/png" href="/img/favicon-16x16.png" sizes="16x16" />
+        <link rel="icon" type="image/png" href="/img/favicon-128.png" sizes="128x128" />
+
         <meta name="application-name" content="&nbsp;"/>
         <meta name="msapplication-TileColor" content="#FFFFFF" />
-        <meta name="theme-color" content="#ffffff">
+        <meta name="msapplication-TileImage" content="/assets/images/icons/mstile-144x144.png" />
+        <meta name="msapplication-square70x70logo" content="/assets/images/icons/mstile-70x70.png" />
+        <meta name="msapplication-square150x150logo" content="/assets/images/icons/mstile-150x150.png" />
+        <meta name="msapplication-wide310x150logo" content="/assets/images/icons/mstile-310x150.png" />
+        <meta name="msapplication-square310x310logo" content="/assets/images/icons/mstile-310x310.png" />
+        <meta name="p:domain_verify" content="6eae6b0d19fa67ad308a2195191cfb44"/>
 
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
