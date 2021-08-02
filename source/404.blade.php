49ac2d7a6c2f83844ce30e5f2af6b17272a46cd2
Jethro May <jethro.may@minttwist.com>
2021-08-02 15:07:07 +0200

    Removed content from 404

--- source/404.blade.php
+++ source/404.blade.php
@@ -11,11 +11,5 @@ permalink: 404.html
         <h1 class="text-6xl font-light leading-none mb-2">404</h1>
 
         <h2 class="text-3xl">Page not found.</h2>
-
-        <hr class="block w-full max-w-sm mx-auto border my-8">
-
-        <p class="text-xl">
-            Need to update this page? See the <a title="404 Page Documentation" href="https://jigsaw.tighten.co/docs/custom-404-page/">Jigsaw documentation</a>.
-        </p>
     </div>
 @endsection
