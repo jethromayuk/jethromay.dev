f25179fb4f00fe590b5c4daf9a9e8cd2be529967
Jethro May <jethro.may@minttwist.com>
2021-12-12 10:26:51 +0200

    Cleaned up tailwind.config.js file in line with tailwindcss 3.0

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,48 +1,26 @@
-const colors = require('tailwindcss/colors')
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
-  mode: 'jit',
-  important: true,
-  purge: [
-    'source/**/*.html',
-    'source/**/*.md',
-    'source/**/*.js',
-    'source/**/*.php',
-    'source/**/*.vue',
-  ],
-  options: {
-    whitelist: [
-      /hljs/,
+    content: [
+        'source/**/*.html',
+        'source/**/*.md',
+        'source/**/*.js',
+        'source/**/*.php',
+        'source/**/*.vue'
     ],
-  },
-  theme: {
-    extend: {
-        colors: {
-            madison: '#2d3e50',
-            niagara: {
-                light: '#1bb698',
-                DEFAULT: '#17a085'
+    theme: {
+        extend: {
+            colors: {
+                madison: '#2d3e50',
+                niagara: {
+                    light: '#1bb698',
+                    DEFAULT: '#17a085'
+                }
+            },
+            fontFamily: {
+                sans: ['Karla', ...defaultTheme.fontFamily.sans],
             }
         },
-        fontFamily: {
-            sans: ['Karla', ...defaultTheme.fontFamily.sans],
-        }
     },
-    theme: {
-        colors: {
-          transparent: 'transparent',
-          current: 'currentColor',
-          black: colors.black,
-          white: colors.white,
-          gray: colors.trueGray,
-          blue: colors.blue,
-          green: colors.green,
-        }
-      }
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-};
+    plugins: [],
+}
