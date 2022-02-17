d32184d7168b44f5600e6ae0da9f02a2a5c449b4
Jethro May <jethro.may@minttwist.com>
2022-02-17 10:29:35 +0200

    Rebuilt and updated twitter tag in head

--- torchlight.php
+++ torchlight.php
@@ -3,10 +3,10 @@
 return [
     // Which theme you want to use. You can find all of the themes at
     // https://torchlight.dev/themes, or you can provide your own.
-    'theme' => 'material-theme-default',
+    'theme' => 'material-theme-palenight',
 
     // Your API token from torchlight.dev. You can set it as an ENV variable
-    // (shown below), or just hardcode it if your repo is private.
+    // (shown below), or just hardcode it if your repo is private.  
     'token' => getenv('TORCHLIGHT_API_TOKEN'),
 
     // If you want to register the blade directives, set this to true.
@@ -19,12 +19,12 @@ return [
     // that you should *not* use the same path that Jigsaw uses,
     // which is `cache` at the root level of your app.
     'cache_path' => 'torchlight_cache',
-
-    // Because of the way Jigsaw works as a static site generator, all the
-    // code blocks for your entire site will be sent as one request. We
+    
+    // Because of the way Jigsaw works as a static site generator, all the 
+    // code blocks for your entire site will be sent as one request. We 
     // increase the timeout to 15 seconds to cover for that.
     'request_timeout' => 15,
-
+    
     // Global options to control blocks-level settings.
     // https://torchlight.dev/docs/options
     'options' => [
